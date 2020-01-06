2805d9e284e5e8604e5a01fa17374b4235d31fe8
hyunsik won <shik0601@gmail.com>
2020-01-06 19:20:21 +0900

    #11654 문제 품

---
+++ Baekjoon/문자열/아스키 코드_#11654/아스키 코드_#11654/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  아스키 코드_#11654
+//
+//  Created by 원현식 on 2020/01/06.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    char a;
+    
+    cin >> a;
+    
+    cout << a - 0 << '\n';
+    
+}
