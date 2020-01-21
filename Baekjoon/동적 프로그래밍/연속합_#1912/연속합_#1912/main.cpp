615818c0abf9197ce69b86dd8bdc49ab8c22e992
hyunsik won <shik0601@gmail.com>
2020-01-21 21:13:39 +0900

    #1912 번 품. 정답률 낮다고 나오는데 얜 또 쉽게 품...

---
+++ Baekjoon/동적 프로그래밍/연속합_#1912/연속합_#1912/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  연속합_#1912
+//
+//  Created by 원현식 on 2020/01/21.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    
+    int a[100001];
+    int dp[100001];
+    
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        a[i] = value;
+    }
+    
+    
+    dp[0] = a[0];
+    int maxValue = a[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (a[i] < a[i-1] ) {
+            dp[i] = dp[i-1] + a[i];
+        } else {
+            dp[i] = max(max(dp[i-1] + a[i], a[i] + a[i-1]), a[i]);
+        }
+        if (dp[i] > maxValue) {
+            maxValue = dp[i];
+        }
+        
+    }
+    
+    cout << maxValue << '\n';
+    
+}
