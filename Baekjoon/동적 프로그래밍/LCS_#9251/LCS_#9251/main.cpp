2a043d2889f7dec6f04b6959bf17c96dc853cf9a
hyunsik won <shik0601@gmail.com>
2020-01-21 17:23:34 +0900

    모르겠다 나중에 다시 풀어보자...

---
+++ Baekjoon/동적 프로그래밍/LCS_#9251/LCS_#9251/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  LCS_#9251
+//
+//  Created by 원현식 on 2020/01/20.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    string str1;
+    string str2;
+    
+    cin >> str1 >> str2;
+
+}
