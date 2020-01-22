51b9af9f5a5ba48ecb18ea44b7b098fb5f4edec2
hyunsik won <shik0601@gmail.com>
2020-01-22 21:55:12 +0900

    이해 안된다...

---
+++ Baekjoon/동적 프로그래밍/평범한 배낭_#12865/평범한 배낭_#12865/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  평범한 배낭_#12865
+//
+//  Created by 원현식 on 2020/01/22.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int n,k;
+int d[100001];  // 가방 용량
+int w[101];     // 물건의 무게
+int v[101];     // 물건의 가치
+
+void dp(){
+    for(int i=1; i<=n; i++){
+        for(int j=k; j>=1; j--){
+            if(w[i] <= j){
+                d[j] = max(d[j], d[j-w[i]] + v[i]);
+            }
+        }
+    }
+    
+    cout << d[k] << "\n";
+}
+
+int main(int argc, const char * argv[]) {
+    
+    cin >> n >> k;
+    for(int i=1; i<=n; i++){
+        cin >> w[i] >> v[i];
+    }
+    
+    dp();
+    
+    return 0;
+}
