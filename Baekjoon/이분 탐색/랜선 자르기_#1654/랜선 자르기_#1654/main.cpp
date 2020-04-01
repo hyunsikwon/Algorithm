41d18a5f90869864ecf30bb4ed7a620f2904e0f9
hyunsik won <shik0601@gmail.com>
2020-04-01 21:31:38 +0900

    어렵다... 다음에 해보자...

---
+++ Baekjoon/이분 탐색/랜선 자르기_#1654/랜선 자르기_#1654/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  랜선 자르기_#1654
+//
+//  Created by 원현식 on 2020/04/01.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int K,N;
+int A[10001];
+int sum;
+
+int main(int argc, const char * argv[]) {
+    cin >> K >> N;
+    
+    for (int i = 0; i < K; i++) {
+        int a;
+        cin >> a;
+    }
+
+}
