8e21b89966d7def3fba4bf05ec65644227c50ea0
hyunsik won <shik0601@gmail.com>
2020-04-23 15:57:38 +0900

    programmers 초기 커밋

---
+++ Programmers/해시/완주하지 못한 선수/완주하지 못한 선수/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  완주하지 못한 선수
+//
+//  Created by 원현식 on 2020/04/23.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> participant;
+vector<string> completion;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    
+    return answer;
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+}
