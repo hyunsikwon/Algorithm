da96d2f429e111918a21b4fabacb5bf2f568f341
hyunsik won <shik0601@gmail.com>
2020-04-23 17:16:14 +0900

    품

--- Programmers/해시/전화번호 목록/전화번호 목록/main.cpp
+++ Programmers/해시/전화번호 목록/전화번호 목록/main.cpp
@@ -9,16 +9,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size() - 1; i++) {
+        if (phone_book[i].length() < phone_book[i+1].length()) {
+            bool isSame = true;
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (phone_book[i][j] != phone_book[i+1][j]) {
+                    isSame = false;
+                    continue;
+                }
+            }
+            if (isSame) return false;
+            
+        }
+    }
     
     return answer;
-}
-
-int main(void) {
-    
 }
