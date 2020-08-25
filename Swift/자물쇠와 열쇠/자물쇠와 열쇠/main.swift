af217e6a5291ad9b4f3f722f7d33ad7ce77cec09
hyunsik won <shik0601@gmail.com>
2020-08-25 17:47:40 +0900

    도움받아 품, 이해함

--- Swift/자물쇠와 열쇠/자물쇠와 열쇠/main.swift
+++ Swift/자물쇠와 열쇠/자물쇠와 열쇠/main.swift
@@ -16,10 +16,17 @@ print(solution(key, lock))
 
 func solution(_ key:[[Int]], _ lock:[[Int]]) -> Bool {
     
+    let range = -(key.count - 1)..<lock.count
+    
     var rotatedKey = key
     for _ in 0..<4 {
-        // sum
-        // check - return true
+        for row in range {
+            for col in range {
+                if check(key: rotatedKey, lock: lock, to: (row: row, col: col)) {
+                    return true
+                }
+            }
+        }
         
         rotatedKey = rotate(rotatedKey)
     }
@@ -27,24 +34,51 @@ func solution(_ key:[[Int]], _ lock:[[Int]]) -> Bool {
     return false
 }
 
+func check(key: [[Int]], lock: [[Int]], to index: (row: Int, col: Int) ) -> Bool {
+    
+    var tempLock = lock
+    for r in 0..<key.count {
+        for c in 0..<key.count {
+            let lockRow = index.row + r
+            let lockCol = index.col + c
+            
+            guard lockRow >= 0, lockCol >= 0, lockRow < lock.count, lockCol < lock.count else {
+                continue
+            }
+            
+            if key[r][c] == 1 && lock[lockRow][lockCol] == 1 { return false }
+            
+            guard key[r][c] == 1 &&  lock[lockRow][lockCol] == 0 else { continue }
+
+            
+            tempLock[lockRow][lockCol] = 1
+                        
+        }
+    }
+
+    return isOpen(tempLock)
+}
+
 func rotate(_ key: [[Int]]) -> [[Int]]{
     let n = key.count
     var rotated = [[Int]](repeating: Array(repeating: 0, count: n), count: n)
-  
+
     for i in 0..<n {
         for j in 0..<n {
             rotated[i][j] = key[n-j-1][i]
         }
     }
+    
     return rotated
 }
 
-func check(_ lock: [[Int]]) -> Bool {
+func isOpen(_ lock: [[Int]]) -> Bool {
     
-    for line in lock {
-        if line.contains(0) || line.contains(2) {
+    for row in lock {
+        if row.contains(0){
             return false
         }
     }
+    
     return true
 }
