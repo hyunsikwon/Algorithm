3daa7b9b755f543dd532cd8b0a4fca81370f7ef6
hyunsik won <shik0601@gmail.com>
2020-09-19 23:05:36 +0900

    백트래킹 폴더 정리

---
+++ Baekjoon/백트래킹/N과 M(2)/N과 M(2)/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  N과 M(2)
+//
+//  Created by 원현식 on 2020/09/19.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ")
+
+let N = Int(input[0])!
+let M = Int(input[1])!
+
+
+
