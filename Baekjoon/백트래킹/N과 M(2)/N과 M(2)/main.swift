fa1dd8f552a57d15d604f37518dde6cf55b8b756
hyunsik won <shik0601@gmail.com>
2020-09-20 23:45:09 +0900

    2020.09.20 품

--- Baekjoon/백트래킹/N과 M(2)/N과 M(2)/main.swift
+++ Baekjoon/백트래킹/N과 M(2)/N과 M(2)/main.swift
@@ -8,10 +8,30 @@
 
 import Foundation
 
+func backTracking(_ depth: Int, _ start: Int){
+    if depth == M {
+        for j in 0..<M {
+            print(arr[j], terminator: " ")
+        }
+        print()
+        return
+    }
+    
+    for i in start..<N {
+        arr[depth] = i+1
+        backTracking(depth+1, i+1)
+        
+    }
+    
+}
+
 let input = readLine()!.split(separator: " ")
 
+
+var arr = [Int](repeating: 0, count: 9)
 let N = Int(input[0])!
 let M = Int(input[1])!
 
+backTracking(0,0)
 
 
